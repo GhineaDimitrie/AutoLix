65a90193f36e54c6091acf67d31f972f1369d10e
Ghinea <ghineadimi@gmail.com>
2026-03-09 11:56:46 +0200

    Changed the login redirect

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/marketplace", true)
+                        .defaultSuccessUrl("/homepage", true)
                         .permitAll()
                 )
 
